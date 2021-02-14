e49aea35ab076ca5ae5b756981a06800cecfe7a7
vscav <vscavinner@gmail.com>
2021-02-15 00:26:55 +0100

    TD02 - Exercise 3 - Questions 3 & 4

--- TD02/src/RegEx.java
+++ TD02/src/RegEx.java
@@ -1,14 +1,9 @@
 import java.util.regex.Pattern;
+import java.util.Arrays;
+// import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 
 public class RegEx {
-
-	public static boolean isNumber(String s) {
-		Pattern p = Pattern.compile("[0-9]+");
-		Matcher m = p.matcher(s);
-		
-		return m.matches();
-	}
 
 	public static void main(String[] args) {
 		// Q.01
@@ -30,9 +25,17 @@ public class RegEx {
 		// 1234
 		// 10
 
+		System.out.println("\nQ.02\n");
+
 		if (args.length > 0) {
+			String regex = "[0-9]+";
+			Pattern p = Pattern.compile(regex);
+			Matcher m;
 			for (String arg : args) {
-				if (isNumber(arg)) {
+				m = p.matcher(arg);
+				// MatchResult result = m.toMatchResult();
+				// System.out.println("Current Matcher: " + result);
+				if (m.matches()) { // tries to match the whole string against the pattern
 					System.out.println(arg);
 				}
 			}
@@ -45,6 +48,62 @@ public class RegEx {
 		// 3
 		// 789
 
+		System.out.println("\nQ.03\n");
+
+		if (args.length > 0) {
+			String regex = "^\\D*(\\d+)$";
+			Pattern p = Pattern.compile(regex);
+			Matcher m;
+			for (String arg : args) {
+				m = p.matcher(arg);
+				// MatchResult result = m.toMatchResult();
+				// System.out.println("Current Matcher: " + result);
+				while (m.find()) { // jump to the next occurrence
+					System.out.println(m.group(1));
+				}
+			}
+		}
+
+		// Q.04
+
+		// Arguments: 127.0.0.1 108.9.98.3 234.0.32.11.34 112.28.255
+		// Expected outputs: 
+		// [127, 0, 0, 1]
+		// [108, 9, 98, 3]
+
+		System.out.println("\nQ.04\n");
+
+		for (String arg : args) {
+			try {
+				int[] ip = parseIpv4(arg);
+				if (ip != null) {
+					System.out.println(Arrays.toString(ip));
+				}
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	public static int[] parseIpv4(final String string) throws Exception {
+		String regex = "^(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})$";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(string);
+
+		int[] ipAddress = null;
+		if (m.find()) {
+			ipAddress = new int[4];
+			
+			ipAddress[0] = Integer.parseInt(m.group(1));
+			ipAddress[1] = Integer.parseInt(m.group(2));
+			ipAddress[2] = Integer.parseInt(m.group(3));
+			ipAddress[3] = Integer.parseInt(m.group(4));
+		}
+
+		return ipAddress;
 	}
 
 }
